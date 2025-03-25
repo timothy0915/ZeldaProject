199b49cb353fdfcb4c03ae6bc649a068dd3cfcbb
timothy0915 <timothy85092356@gmail.com>
2025-03-25 19:23:49 +0800

    新41
    小修改粒子特效
    同時把菇菇ATK都改成1

--- Assets/Scripts/Player/AutoDestroy.cs
+++ Assets/Scripts/Player/AutoDestroy.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AutoDestroy : MonoBehaviour
-{ 
-        void Start()
-        {
-            Destroy(gameObject, 2f);
-        }
+{
+    void Start()
+    {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        // 单采╰参冀丁綪反ン
+        Destroy(gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
+    }
 }
